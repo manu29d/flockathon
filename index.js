2c306a8c2fba3bac6537d59b3b26dd321145ef1d
Manu Dwivedi <manu29.d@gmail.com>
2017-02-18 18:43:41 +0530

    WIP: v0

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var flock = require('flockos');
 var config = require('./config.js');
 var express = require('express');
 var fs = require('fs');
+var request = require('request');
+var ejs = require('ejs');
 
 flock.setAppId(config.appId);
 flock.setAppSecret(config.appSecret);
@@ -10,7 +12,34 @@ var app = express();
 
 // Listen for events on /events, and verify event tokens using the token verifier.
 app.use(flock.events.tokenVerifier);
+app.set('view engine', 'ejs');
+
 app.post('/events', flock.events.listener);
+app.get('/actions', function (req, res, next) {
+  var event = JSON.parse(req.query.flockEvent);
+  flock.callMethod('chat.fetchMessages', tokens[event.userId], {
+    chat: event.chat,
+    uids: [event.messageUids.messageUid]
+  }, function (err, messages) {
+    if (err) { return console.log(err) }
+    else {
+      request({
+        uri: 'https://www.googleapis.com/customsearch/v1',
+        qs: {
+          key: config.google.key,
+          cx: config.google.cx,
+          q: messages[0].text
+        }
+      }, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+          res.render('actions', {items: JSON.parse(body).items});
+        } else {
+          console.log(error);
+        }
+      })
+    }
+  });
+});
 
 // Read tokens from a local file, if possible.
 var tokens;
@@ -30,8 +59,13 @@ flock.events.on('app.uninstall', function (event) {
     delete tokens[event.userId];
 });
 
+flock.events.on('client.messageAction', function (event) {
+});
+
+
+
 // Start the listener after reading the port from config
-var port = config.port || 8080;
+var port = config.port || 9000;
 app.listen(port, function () {
     console.log('Listening on port: ' + port);
 });
